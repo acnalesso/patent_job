3d27031a5601f885a83199c9a3af8bb67775dd07
Antonio C Nalesso Moreira <acnalesso@yahoo.co.uk>
2013-05-06 01:53:19 +0100

    deleted one test

--- spec/patent_job/patent_job_spec.rb
+++ spec/patent_job/patent_job_spec.rb
@@ -7,12 +7,6 @@ describe PatentJob do
     @file = "data.csv"
     @job.stub(:update_patents).with(anything).and_return(true)
   end
-  it "should download the csv file from the ftp server" do
-    @job.should_receive(:download_file).and_return(@file)
-    f = File.read(@job.download_file)
-
-    f.include?('just 3 minutes').should be_true
-  end
 
   it "should replace existing patents with new patents" do
     @job.should_receive(:download_file).and_return(@file)
