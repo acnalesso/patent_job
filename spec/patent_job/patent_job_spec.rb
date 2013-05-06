5a222cb31ee40d9058e0e13111a69951ed66462f
Antonio C Nalesso Moreira <acnalesso@yahoo.co.uk>
2013-05-06 01:42:12 +0100

    fake it, until you make it

--- spec/patent_job/patent_job_spec.rb
+++ spec/patent_job/patent_job_spec.rb
@@ -1,17 +1,17 @@
+$:.unshift(File.expand_path('../../../',__FILE__))
 require 'spec_helper'
 
 describe PatentJob do
   before do
     @job = PatentJob.new
-    @file = '/tmp/data.csv'
+    @file = "data.csv"
     @job.stub(:update_patents).with(anything).and_return(true)
   end
   it "should download the csv file from the ftp server" do
     @job.should_receive(:download_file).and_return(@file)
-
     f = File.read(@job.download_file)
-    f.include?("just 3 minutes").should be_true
 
+    f.include?('just 3 minutes').should be_true
   end
 
   it "should replace existing patents with new patents" do
